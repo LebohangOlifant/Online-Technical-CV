4ec29c29df455ea99ea6ea931ec2e9c9c0a71ee9
LebohangOlifant <lebohangtladi789@gmail.com>
2020-10-06 04:26:10 +0100

    Aninimated the welcome message using css, Added code to the button, so that it can download CV when clicked.

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Vanderbijlpark&units=degreCelsius&APPID=5d6d862d8577f830164f96a4caba27d5", 
+/*Start of download button functionality*/
+$(document).ready(function(){
+   $('button').click(function(e){
+      e.preventDefault();
+      window.location.href = "CV.pdf"
+   });
+});
+
+/*End of download button functionality*/
+
+
+
+/*Start of API Section*/
+var city = "Vanderbijlpark";
+
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5d6d862d8577f830164f96a4caba27d5", 
          function(data){
             console.log(data);
 
             var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            var temp = data.main.temp;
+            var weather = data.weather[0].main
 
-            console.log(icon);
+            $(".icon").attr("src", icon);
+            $(".temp").append(temp);
+            $(".weather").append(weather);
 });
+/*End of API Section*/
