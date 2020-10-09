045f2b415f197c89068fb9a90c9df76a9edaedf4
LebohangOlifant <lebohangtladi789@gmail.com>
2020-10-09 01:51:22 +0100

    Added Two APIs which makes them three, Added content inside containers so that it is scalable across all screens, added section for projects with all my projects

--- script.js
+++ script.js
@@ -6,11 +6,22 @@ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5
             console.log(data);
 
             var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-            var temp = data.main.temp;
             var weather = data.weather[0].main
-
+            
             $(".icon").attr("src", icon);
-            $(".temp").append(temp);
             $(".weather").append(weather);
 });
 /*End of API Section*/
+
+/* Start of Projects Buttons*/
+function openCity(ProjectName) {
+   var i;
+   var x = document.getElementsByClassName("project-name");
+   for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+   }
+   document.getElementById(ProjectName).style.display = "block";
+ }
+ /*End of projects Buttons*/
+
+ 
